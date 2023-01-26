58f6e0b6f4f18f68b5c6e78339c77ab2a62c626c
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-01-26 14:18:56 +0300

    First Commit. Onion Arch is set using folders, class libs and a web api project. A client also added to consume the api. The arch. was tested using dummy data.

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts() => new()
+        {
+            new() { Id = Guid.NewGuid(), Created = DateTime.Now, Name = "Dummy Product 1", Price= 11, Stock = 13 },
+            new() { Id = Guid.NewGuid(), Created = DateTime.Now, Name = "Dummy Product 2", Price= 21, Stock = 23 },
+            new() { Id = Guid.NewGuid(), Created = DateTime.Now, Name = "Dummy Product 3", Price= 31, Stock = 33 },
+            new() { Id = Guid.NewGuid(), Created = DateTime.Now, Name = "Dummy Product 4", Price= 41, Stock = 43 },
+            new() { Id = Guid.NewGuid(), Created = DateTime.Now, Name = "Dummy Product 5", Price= 51, Stock = 53 }
+        };
+    }
+}
