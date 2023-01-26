e409f7cba4e4965c41c1dee102ca0c33816d619e
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-01-27 01:39:53 +0300

    Services are all added as singleton. It may change in the feature. Respository pattern tested using ProductCommandRepository which is passed successfully.

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -1,5 +1,7 @@
 ﻿
+using ETicaretAPI.Application.Repositories;
 using ETicaretAPI.Persistence.Contexts;
+using ETicaretAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -17,7 +19,13 @@ namespace ETicaretAPI.Persistence
             services.AddDbContext<ETicaretAPIDbContext>(opt =>
             {
                 opt.UseMySQL(Configuration.ConnectionString);
-            });
+            }, ServiceLifetime.Singleton);
+            services.AddSingleton<ICustomerCommandRepository, CustomerCommandRepository>();
+            services.AddSingleton<ICustomerQueryRepository, CustomerQueryRepository>();
+            services.AddSingleton<IOrderCommandRepository, OrderCommandRepository>();
+            services.AddSingleton<IOrderQueryRepository, OrderQueryRepository>();
+            services.AddSingleton<IProductCommandRepository, ProductCommandRepository>();
+            services.AddSingleton<IProductQueryRepository, ProductQueryRepository>();
         }
     }
 }
---
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductCommandRepository productCommand;
+        public ProductController(IProductCommandRepository productCommand)
+        {
+            this.productCommand = productCommand;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await productCommand.AddAsync(new()
+            {
+                Id = Guid.NewGuid(),
+                Created = DateTime.Now,
+                Name = "Test",
+                Price = 31,
+                Stock = 31
+            });
+            await productCommand.SaveAsync();
+            return Ok();
+        }
+    }
+}
