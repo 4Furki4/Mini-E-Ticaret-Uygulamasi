7e525788881e312a769ccb86dbe62d40056970ed
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-01-27 00:11:38 +0300

    Repository pattern is being implemented. Query, Command and a general Repository interface are added and its concretes are implemented. Time to create repository interfaces for each entity.

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/CommandRepository.cs
@@ -0,0 +1,56 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities.Common;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class CommandRepository<T> : ICommandRepository<T> where T : BaseEntity
+    {
+        private readonly ETicaretAPIDbContext context;
+        public CommandRepository(ETicaretAPIDbContext context)
+        {
+            this.context = context;
+        }
+        public DbSet<T> Table => context.Set<T>();
+
+        public async Task<bool> AddAsync(T entity)
+        {
+            EntityEntry<T> entityEntry = await Table.AddAsync(entity);
+            return entityEntry.State == EntityState.Added;
+        }
+
+        public async Task<bool> AddRangeAsync(List<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+            return true;
+        }
+
+        public bool Remove(T entity)
+        {
+            EntityEntry entityEntry = Table.Remove(entity);
+            return entityEntry.State == EntityState.Deleted;
+        }
+
+        public async Task<bool> RemoveAsync(string id)
+        {
+            var entity = await Table.FirstOrDefaultAsync(b => b.Id == Guid.Parse(id));
+            EntityEntry<T> entityEntry = Table.Remove(entity);
+            return entityEntry?.State == EntityState.Deleted;
+        }
+
+        public async Task<int> SaveAsync() => await context.SaveChangesAsync();
+
+        public bool Update(T entity)
+        {
+            EntityEntry<T> entityEntry = Table.Update(entity);
+            return entityEntry.State == EntityState.Modified;
+        }
+    }
+}
---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/QueryRepository.cs
@@ -0,0 +1,36 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public class QueryRepository<T> : IQueryRepository<T> where T : BaseEntity
+    {
+        private readonly ETicaretAPIDbContext context;
+        public QueryRepository(ETicaretAPIDbContext context)
+        {
+            this.context = context;
+        }
+        public DbSet<T> Table =>
+
+            context.Set<T>();
+
+
+        public IQueryable<T> GetAll() => Table;
+
+        
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> expression) =>
+            await Table.FirstOrDefaultAsync(expression);
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> expression) => Table.Where(expression);
+        public async Task<T> GetByIdAsync(string id) =>
+            await Table.FirstOrDefaultAsync(b => b.Id == Guid.Parse(id));
+    }
+}
