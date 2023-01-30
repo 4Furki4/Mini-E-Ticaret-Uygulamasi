c521a41e0b8ee06d40a4859c7ecb185068bf8d6f
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-01-30 04:10:26 +0300

    message method now has Partial<AlertifyOptions> parameter to consume set notificatin settings.

--- ETicaretClient/src/app/admin/layout/layout.component.ts
+++ ETicaretClient/src/app/admin/layout/layout.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import { AlertifyService, AlertType } from 'src/app/services/admin/alertify/alertify.service';
-
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
-
-  constructor(private alertify: AlertifyService) {
-    alertify.message("Hoş geldiniz sayın yönetici!", AlertType.Notify)
-  }
+  constructor() { }
 }
--- ETicaretClient/src/app/services/admin/alertify/alertify.service.ts
+++ ETicaretClient/src/app/services/admin/alertify/alertify.service.ts
@@ -6,16 +6,42 @@ declare var alertify: any;
 export class AlertifyService {
 
   constructor() { }
-
-  message(message: string, alertType: AlertType) {
-    alertify[alertType](message);
+  message(message: string, options: Partial<AlertifyOptions>) {
+    this.setPosition(options.position);
+    this.setDelay(options.delay)
+    const msg = alertify[options.alertType ?? AlertType.Message](message);
+    if (options.dismissOthers)
+      msg.dismissOthers();
+  }
+  setPosition(position: Position | undefined) {
+    alertify.set("notifier", "position", position)
+  }
+  setDelay(delay: number | undefined) {
+    alertify.set("notifier", "delay", delay)
+  }
+  dismissAll() {
+    alertify.dismissAll();
   }
 }
-
+export class AlertifyOptions {
+  alertType!: AlertType;
+  position: Position = Position.BottomLeft;
+  delay: number = 4;
+  dismissOthers: boolean = false;
+}
 export enum AlertType {
   Error = "error",
   Notify = "notify",
   Message = "message",
   Success = "success",
   Warning = "warning"
+}
+
+export enum Position {
+  BottomLeft = "bottom-left",
+  BottomRight = "bottom-right",
+  BottomCenter = "bottom-center",
+  TopLeft = "top-left",
+  TopRight = "top-right",
+  TopCenter = "top-center"
 }
