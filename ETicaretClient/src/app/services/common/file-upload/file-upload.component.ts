8aa02c525dd297746c29ced08f1d2201d36d381a
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-02-12 20:13:04 +0300

    ng-file-upload lib was installed and initiated. htpp request will be prepared.

---
+++ ETicaretClient/src/app/services/common/file-upload/file-upload.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { NgxFileDropEntry } from 'ngx-file-drop';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.scss']
+})
+export class FileUploadComponent {
+  public files: NgxFileDropEntry[] = [];
+
+  public dropped(files: NgxFileDropEntry[]) {
+    this.files = files;
+    let formFile: FormData = new FormData();
+    for (const droppedFile of files) {
+      if (droppedFile.fileEntry.isFile) {
+        const FSfileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        FSfileEntry.file((file: File) => {
+          formFile.append(file.name, file, droppedFile.relativePath);
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+
+          /**
+          // You could upload it like this:
+          const formData = new FormData()
+          formData.append('logo', file, relativePath)
+
+          // Headers
+          const headers = new HttpHeaders({
+            'security-token': 'mytoken'
+          })
+
+          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
+          .subscribe(data => {
+            // Sanitized logo returned from backend
+          })
+          **/
+
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }
+  }
+
+  public fileOver(event: any) {
+    console.log(event);
+  }
+
+  public fileLeave(event: any) {
+    console.log(event);
+  }
+}
