d0008ac5d4b66bcd9b61e29a86149da3de22a530
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-02-25 02:01:30 +0300

    reactive form was prepared with validation regulations.

--- ETicaretClient/src/app/ui/components/register/register.component.ts
+++ ETicaretClient/src/app/ui/components/register/register.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+  form !: FormGroup
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      fullName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      userName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.maxLength(40), Validators.pattern(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[\.\*!_@-])(?=.*[a-zA-Z]).{8,}$/)]],
+      confirmPassword: ['', [Validators.required]]
+    })
+    this.form.value
+  }
+  get fullName() {
+    return this.form.get('fullName');
+  }
+  get userName() {
+    return this.form.get('userName');
+  }
+  get email() {
+    return this.form.get('email');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+  get confirmPassword() {
+    return this.form.get('confirmPassword');
+  }
+
+  onSubmit(value: any) {
+
+  }
 }
--- ETicaretClient/src/app/ui/components/register/register.module.ts
+++ ETicaretClient/src/app/ui/components/register/register.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 import { RouterModule } from '@angular/router';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -13,7 +15,11 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     RouterModule.forChild([
       { path: '', component: RegisterComponent }
-    ])
+    ]),
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class RegisterModule { }
+
