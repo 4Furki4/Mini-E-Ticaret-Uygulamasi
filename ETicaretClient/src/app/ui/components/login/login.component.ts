a97e6c7f933d4784c381618a600157f514d34d20
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-02-26 23:58:34 +0300

    login module, its component and router path were added. A form, that needs more css to do, has validations for its controllers .

---
+++ ETicaretClient/src/app/ui/components/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  form !: FormGroup;
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  get userName() {
+    return this.form.get('userName')
+  }
+  get password() {
+    return this.form.get('password')
+  }
+
+  onSubmit(formValues: any, ngForm: FormGroupDirective) {
+    console.log(formValues);
+    console.log(ngForm);
+  }
+
+}
---
+++ ETicaretClient/src/app/ui/components/login/login.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([{
+      path: '', component: LoginComponent
+    }])
+  ]
+})
+export class LoginModule { }
