a13b0815e893847c9f18ba4683e8ca77d33ce8a7
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-02-27 11:53:16 +0300

    new folder AppUserCommands created and CreateAppUser file for MediatR request, handler and response was added to it. Exceptions folder created and UserCreateFailedException added to it. UsersController was added with AppUser HTTPPOST endpoint with CreateAppUserViewModel.

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Features/Commands/AppUserCommands/CreateAppUser.cs
@@ -0,0 +1,53 @@
+﻿using ETicaretAPI.Application.Exceptions;
+using ETicaretAPI.Application.ViewModels.Identity;
+using ETicaretAPI.Domain.Entities.Identity.AppUsers;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUserCommands
+{
+    public class CreateAppUserCommandRequest : IRequest<CreateAppUserCommandResponse>
+    {
+        public CreateAppUserViewModel ViewModel { get; set; }
+        public CreateAppUserCommandRequest(CreateAppUserViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+    }
+
+    public class CreateAppUserCommandHandler : IRequestHandler<CreateAppUserCommandRequest, CreateAppUserCommandResponse>
+    {
+        readonly UserManager<AppUser> userManager;
+
+        public CreateAppUserCommandHandler(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public async Task<CreateAppUserCommandResponse> Handle(CreateAppUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            AppUser appUser = new()
+            {
+                FullName = request.ViewModel.FullName,
+                Email = request.ViewModel.Email,
+                UserName = request.ViewModel.UserName,
+            };
+            string password = request.ViewModel.Password;
+            IdentityResult result = await userManager.CreateAsync(appUser, password);
+            if (result.Succeeded)
+                return new() { IsSuccessfull = true, Message = "User created successfully!" };
+            else
+                throw new UserCreateFailedException();
+        }
+    }
+
+    public class CreateAppUserCommandResponse
+    {
+        public bool IsSuccessfull { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using ETicaretAPI.Application.Features.Commands.AppUserCommands;
+using ETicaretAPI.Application.ViewModels.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    public class UsersController : Controller
+    {
+        readonly IMediator mediator;
+        public UsersController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> AppUser(CreateAppUserViewModel viewModel)
+        {
+            CreateAppUserCommandRequest request = new(viewModel);
+            CreateAppUserCommandResponse result = await mediator.Send(request);
+            return Ok(result);
+        }
+
+    }
+}
